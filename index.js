ca3ec82b1f4e90363c3d7aca1bccc9a2766e147a
xphou <XuePing.Hou@futurmaster.com>
2018-11-19 14:02:39 +0800

    第三次提交

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-; the project works with npm via internal npm reference
-registry=http://s-tfs1:8080/tfs/RD/_packaging/web-projects/npm/registry
-always-auth=true
+;
+the project works with npm via internal npm reference
+registry = http: //s-tfs1:8080/tfs/RD/_packaging/web-projects/npm/registry
+    always - auth = true
+
+function getFunctionName(name) {
+    var tmp = name.callee.toString();
+    var re = /function\s*(\w*)/i;
+    // var re = /(?<=prototype\.)(.+?)(?=\=)/i;
+    var matches = re.exec(tmp);
+    return matches[1];
+}
