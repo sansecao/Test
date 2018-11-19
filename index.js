0b30ef4215dc2abfffe23d2550df8b3da4db65a0
xphou <XuePing.Hou@futurmaster.com>
2018-11-19 14:05:37 +0800

    第四次提交

--- index.js
+++ index.js
@@ -10,3 +10,32 @@ function getFunctionName(name) {
     var matches = re.exec(tmp);
     return matches[1];
 }
+
+function addItemToTheResult(result, expect, name, message) {
+    //If no message is passed, make the name as the default message
+    var description;
+    if (message) {
+        description = message;
+    } else {
+        description = name;
+    }
+
+    //append test results to the page
+    var item = document.createElement("li");
+    item.setAttribute('data-result', result);
+    item.setAttribute("data-functionName", name);
+    item.setAttribute("data-expect", expect);
+    item.setAttribute("data-message", description);
+
+    //set the style of the results
+    if (result == expect || diffObject(result, expect)) {
+        item.setAttribute('class', "success");
+    } else {
+        item.setAttribute('class', "fail");
+    }
+    var str = "CaseName : " + name + " ; description : " + description + " ; result : " + result + " ; expect : " + expect;
+
+    //to show the meassage in html
+    item.innerHTML = "<p>" + str + "</p>";
+    $(".resultItem")[0].appendChild(item);
+}
